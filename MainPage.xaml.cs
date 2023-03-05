378a232d4caf3b57f49d03111fd9a4c2010320cd
MineTestGaming <73379450+MineTestGaming@users.noreply.github.com>
2023-03-05 12:58:46 +0800

    添加项目文件。

---
+++ MainPage.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.ViewManagement;
+using Windows.UI.WindowManagement;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x804 上介绍了“空白页”项模板
+
+namespace Clock
+{
+    /// <summary>
+    /// 可用于自身或导航至 Frame 内部的空白页。
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        
+        
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+        }
+
+
+
+        private async void SwitchFloatWindow_Click(object sender, RoutedEventArgs e)
+        {
+            var WindowMode = ApplicationView.GetForCurrentView();
+            var WindowPrefrence = ViewModePreferences.CreateDefault(ApplicationViewMode.CompactOverlay);
+            WindowPrefrence.CustomSize = new Windows.Foundation.Size(410, 212);
+            if (WindowMode.ViewMode != ApplicationViewMode.CompactOverlay) {
+                 await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.CompactOverlay, WindowPrefrence);
+                return;
+            }
+
+            if(WindowMode.ViewMode == ApplicationViewMode.CompactOverlay)
+            {
+                await ApplicationView.GetForCurrentView().TryEnterViewModeAsync(ApplicationViewMode.Default);
+                return;
+            }
+
+
+        }
+
+        private async void ClockStart()
+        {
+            for (; ; ) {
+                var currentTime = System.DateTime.Now;
+                int Hr = currentTime.Hour;
+                int Min = currentTime.Minute;
+                int Sec = currentTime.Second;
+                TimeTextBlock.Text = Hr.ToString() + ":" + Min.ToString()/* + ":" + Sec.ToString()*/ ;
+                await Task.Delay(100);
+            }
+        }
+
+        private void Page_Loaded(object sender, RoutedEventArgs e)
+        {
+            ClockStart();
+        }
+    }
+}
